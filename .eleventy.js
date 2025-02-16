7ceccf4ccb7c1e27a545fa0d163f81c5fcfebe0a
Sahana Balaji <114191735+Sana47443@users.noreply.github.com>
2025-02-16 23:46:17 +0000

    Tutorial on CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
 
 // The export statement makes these settings available to other files in 11ty
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
-};
 
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("site/images");
   eleventyConfig.addPassthroughCopy("styles");
 
   return {
